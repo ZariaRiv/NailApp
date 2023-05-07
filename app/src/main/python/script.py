6aa63cd9939e616e31ca4aa046fc8d67bb85a4f6
ZariaRiv <k.a.kleczek@tilburguniversity.edu>
2023-05-07 15:29:11 +0200

    cannot open the model, libraries importing

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -4,7 +4,7 @@ import cv2
 
 def run_model(image_path):
     # load the trained model
-    model = tf.keras.models.load_model('model.h5')
+    model = tf.keras.models.load_model('C:/Users/Kasia/Documents/GitHub/NailApp/app/src/main/python/shitty_model.h5')
 
     # load the input image
     img = cv2.imread(image_path)
